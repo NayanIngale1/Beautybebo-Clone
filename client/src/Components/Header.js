96a7344c2a79169265404753f513792b44696cd5
Brajesh Kumar Saxena <95098581+brajeshsaxena01@users.noreply.github.com>
2022-05-05 22:37:50 +0530

    comt_4

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -7,6 +7,10 @@
 //   Form,
 //   FormControl,
 //   Button,
+//   SplitButton,
+//   DropdownButton,
+//   Dropdown,
+//   ButtonGroup,
 // } from 'react-bootstrap';
 
 // export const Header = () => {
@@ -22,9 +26,19 @@
 //               style={{ maxHeight: '100px' }}
 //               navbarScroll
 //             >
-//               <Nav.Link href="#action1">Home</Nav.Link>
-//               <Nav.Link href="#action2">Link</Nav.Link>
-//               <NavDropdown title="Link" id="navbarScrollingDropdown">
+//               {/* <Nav.Link href="#action1">Home</Nav.Link>
+//               <Nav.Link href="#action2">Link</Nav.Link> */}
+
+//               <Nav.Link href="#" disabled>
+//                 Link
+//               </Nav.Link>
+//             </Nav>
+//             <Form className="d-flex" style={{ border: '1px solid red' }}>
+//               <NavDropdown
+//                 title="Link"
+//                 id="navbarScrollingDropdown"
+//                 style={{ border: 'red' }}
+//               >
 //                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
 //                 <NavDropdown.Item href="#action4">
 //                   Another action
@@ -34,21 +48,16 @@
 //                   Something else here
 //                 </NavDropdown.Item>
 //               </NavDropdown>
-//               <Nav.Link href="#" disabled>
-//                 Link
-//               </Nav.Link>
-//             </Nav>
-//             <Form className="d-flex">
-//               <input style={{}} />
+//               <span>|</span>
+//               <FormControl
+//                 type="search"
+//                 placeholder="Search"
+//                 className="me-2"
+//                 aria-label="Search"
+//               />
+
 //               <Button variant="outline-success">Search</Button>
 //             </Form>
-//             <Nav>
-//               <h1>
-//                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-//                   <path d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z" />
-//                 </svg>
-//               </h1>
-//             </Nav>
 //           </Navbar.Collapse>
 //         </Container>
 //       </Navbar>
@@ -85,10 +94,14 @@ export const Header = () => {
           </form>
         </div>
         <div className="main-nav-profile">
-          <div className="like-icon">a</div>
+          <div className="like-icon">
+            <i class="fa-solid fa-heart"></i>
+          </div>
           <div className="user-icon">
-            <div className="a">a</div>
-            <div>My acount</div>
+            <div className="user-icon-pic">
+              <i class="fa fa-user" aria-hidden="false"></i>
+            </div>
+            <span>My Account</span>
           </div>
         </div>
       </div>
