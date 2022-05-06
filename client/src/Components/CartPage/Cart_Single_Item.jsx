b83e5a2588c62d91772113f27322aeb8195eebc5
adarshakhatua <adarshakhatua.ak@gmail.com>
2022-05-06 19:40:57 +0530

    responsiveness

--- client/src/Components/CartPage/Cart_Single_Item.jsx
+++ client/src/Components/CartPage/Cart_Single_Item.jsx
@@ -3,13 +3,22 @@ export const CartSingleItem = () => {
     return (
         <div className="Cart_Item_info">
             <div className="Upper_Content">
-                <div className="Cart_Img_Div">
-                    <img src="https://www.beautybebo.com/pub/media/catalog/product/cache/c9615af5e5a6f27d0b9239c1928d8610/5/4/54_2.jpg" alt="" />
+                <div className="image_title">
+                    <div className="Cart_Img_Div">
+                        <img src="https://www.beautybebo.com/pub/media/catalog/product/cache/c9615af5e5a6f27d0b9239c1928d8610/5/4/54_2.jpg" alt="" />
+                    </div>
+                    <div className="Cart_Title_Div">FACES CANADA PEACHES N CREAM TINTED MOISTURIZER - LIGHT 01</div>
                 </div>
-                <div className="Cart_Title_Div">FACES CANADA PEACHES N CREAM TINTED MOISTURIZER - LIGHT 01</div>
+                <div className="PriceHead">
+                    <div id="price_head">PRICE</div>
+                    <div id="qty_head">QTY</div>
+                    <div id="subtotal_head">SUBTOTAL</div>
+                </div>
                 <div className="CartPriceDiv">
                     <div className="Cart_Price_Div">₹539.00</div>
-                    <div className="Cart_Qty_Div">1</div>
+                    <div className="Cart_Qty_Div">
+                        <input type="text" defaultValue={1}/>
+                    </div>
                     <div className="Cart_Subtotal_Div">₹539.00</div>
                 </div>
             </div>
