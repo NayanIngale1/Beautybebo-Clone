818516bc5ced0eb30d71b4ff3d8183e8c3f0c077
Ritik Gupta <ritikadvice@gmail.com>
2022-05-07 11:06:13 +0530

    Home page added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import { CheckoutPage } from "./Components/Checkout/CheckoutPage";
+import { Merge } from "./Components/homepage/mergecomponet";
 import { ProductDeails } from "./Components/ProductDetails/ProductDeails";
 import { ProductsPage } from "./Components/ProductsPage/ProductsPage";
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element = {<Merge/>}/>
         <Route path="/details/:id" element={<ProductDeails />} />
         <Route path="/products/:type" element={<ProductsPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
---
+++ client/src/Components/homepage/sidebar.jsx
@@ -0,0 +1,69 @@
+export const Sidebar =()=>{
+    return(<><div className='Allcattext'>
+        <span>  <i class="fa-solid fa-list-ul fa-sm"></i></span> ALL CATEGORIES
+    </div>
+    <div className='icondiv'>
+            <div className='iconimage'>
+                <img src="https://www.beautybebo.com/pub/media/wysiwyg/menu-icons/makeup-small.png" alt="" srcset="" />
+                <div>Makeup</div>
+            </div>
+            <div>
+                <i class="fa-solid fa-angle-right"></i>
+            </div>
+        </div>
+        <div className='icondiv'>
+            <div className='iconimage'>
+                <img src="https://www.beautybebo.com/pub/media/wysiwyg/menu-icons/skin-small.png" alt="" srcset="" />
+                <div>Skin</div>
+            </div>
+            <div>
+                <i class="fa-solid fa-angle-right"></i>
+            </div>
+        </div>
+        <div className='icondiv'>
+            <div className='iconimage'>
+                <img src="https://www.beautybebo.com/pub/media/wysiwyg/menu-icons/hair-small.png" alt="" srcset="" />
+                <div>Hair</div>
+            </div>
+            <div>
+                <i class="fa-solid fa-angle-right"></i>
+            </div>
+        </div>
+        <div className='icondiv'>
+            <div className='iconimage'>
+                <img src="https://www.beautybebo.com/pub/media/wysiwyg/menu-icons/personal-care-small.png" alt="" srcset="" />
+                <div>Personal Care</div>
+            </div>
+            <div>
+                <i class="fa-solid fa-angle-right"></i>
+            </div>
+        </div>
+        <div className='icondiv'>
+            <div className='iconimage'>
+                <img src="https://www.beautybebo.com/pub/media/wysiwyg/menu-icons/mom-baby-care-small.png" alt="" srcset="" />
+                <div>Mom & Baby Care</div>
+            </div>
+            <div>
+                <i class="fa-solid fa-angle-right"></i>
+            </div>
+        </div>
+        <div className='icondiv'>
+            <div className='iconimage'>
+                <img src="https://www.beautybebo.com/pub/media/fragrance.png" alt="" srcset="" />
+                <div>Fragrance</div>
+            </div>
+           
+        </div>
+        <div className='icondiv'>
+            <div className='iconimage'>
+                <img src="https://www.beautybebo.com/pub/media/ayurveda.png" alt="" srcset="" />
+                <div>Ayurveda</div>
+            </div>
+           
+        </div>
+        
+        
+        </>
+          
+    )
+}
