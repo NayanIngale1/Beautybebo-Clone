ee23279610c7168683878c11678750a7a2b52d6f
Adarsha Khatua <97473442+adarshakhatua@users.noreply.github.com>
2022-05-08 17:23:43 +0530

    Navbar initial

---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import { BottomNav } from "./Bottom_nav"
+import { MiddleLogoDiv } from "./Middle_logo_div"
+import { TopImageDiv } from "./Top_image"
+
+export const Navbar = () => {
+    return (
+        <>
+            <TopImageDiv></TopImageDiv>
+            <MiddleLogoDiv></MiddleLogoDiv>
+            <BottomNav></BottomNav>
+        </>
+    )
+}
